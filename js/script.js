ed3c043500daa47df5296de5b4ce45f63cc3d315
thetoothless <ninaling@ucla.edu>
2017-01-06 17:07:19 +0800

    basic demo done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
   window.addEventListener("keypress", checkKeyPressed, false);
-
+  
   var pizzaSnap = Snap('#pizza');
   
   var sauceSnap = pizzaSnap.select('#sauce-drop');
   sauceSnap.attr({transform: 't0 -180'});
+  var sauceSnapGroup = pizzaSnap.group(sauceSnap);
   
   var cheeseSnap = pizzaSnap.select('#cheese-drop');
   cheeseSnap.attr({transform: 't0 -180'});
+  var cheeseSnapGroup = pizzaSnap.group(cheeseSnap);
+  cheeseSnapGroup.before(sauceSnapGroup);
+  
+  var pepperoniSnapArr = pizzaSnap.selectAll('path[class^="pepperoni"]');
+  pepperoniSnapArr.attr({transform: 't0 -180'});
+  var pepperoniSnapGroup = pizzaSnap.group(pepperoniSnapArr);
+  pepperoniSnapGroup.before(cheeseSnapGroup);
   
   function checkKeyPressed(e) {
     switch(e.keyCode){
@@ -16,7 +24,6 @@ $(document).ready(function(){
           sauceSnap.animate({opacity: 1, transform: 't0 0'}, 350, function(){
             sauceSnap.animate({d: dropIntermediatePath}, 200, function(){
               var sauceMaskSnap = pizzaSnap.path(sauceMask).attr({id: 'sauce-mask', fill: '#ffffff'});
-              var sauceSnapGroup = pizzaSnap.group(sauceSnap);
               sauceSnapGroup.attr({mask: sauceMaskSnap});
               sauceSnap.animate({transform: 's3'}, 800);
             });
@@ -26,12 +33,14 @@ $(document).ready(function(){
           cheeseSnap.animate({opacity: 1, transform: 't0 0'}, 350, function(){
             cheeseSnap.animate({d: dropIntermediatePath}, 200, function(){
               var cheeseMaskSnap = pizzaSnap.path(cheeseMask).attr({id: 'cheese-mask', fill: '#ffffff'});
-              var cheeseSnapGroup = pizzaSnap.group(cheeseSnap);
               cheeseSnapGroup.attr({mask: cheeseMaskSnap});
               cheeseSnap.animate({transform: 's5.5'}, 1200);
               cheeseAnimation(cheeseMaskSnap);
             });
           });
+        }
+        else if (pepperoniSnapArr[0].attr('opacity') == '0'){
+          pepperoniSnapArr.animate({opacity: 1, transform: 't0 0'}, 500);
         }
         break;
     }
